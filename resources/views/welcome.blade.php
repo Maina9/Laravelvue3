f9ea5961d66673d77ab8286c78593d678093d14a
maina9 <roftymaina96@gmail.com>
2022-05-30 22:03:07 +0300

    inti commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+       <title>Laravel Vue.js</title>
+
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device.width, initial-scaale=1, shrink-to-fit=no">
+    </head>
+    
+</html>
